891e12c58e8dd23a865f9ad05226230ceaff0d1e
lorenzocoracini <lorenzo291909@gmail.com>
2024-04-18 18:54:44 -0300

    Criação de componente de usuários por origem.

--- src/components/UserByOrigin.vue
+++ src/components/UserByOrigin.vue
@@ -1,13 +1,44 @@
 <template>
-  <h1> usuario por origem</h1>
+  <v-card>
+    <v-card-title>Resumo da Distribuição por Origem</v-card-title>
+    <v-card-text>
+      <v-container>
+        <v-row v-for="(count, origin) in userCountByOrigin" :key="origin">
+          <v-col cols="6">{{ origin }}</v-col>
+          <v-col cols="6">{{ count }}</v-col>
+        </v-row>
+      </v-container>
+    </v-card-text>
+  </v-card>
 </template>
 
 <script>
-export default {
+import { useUsersStore } from '@/stores/users';
+import { defineComponent, computed } from 'vue';
 
-}
+export default defineComponent({
+  setup() {
+    const usersStore = useUsersStore();
+
+    const userCountByOrigin = computed(() => {
+      const users = usersStore.users;
+      const countByOrigin = {};
+
+      users.forEach(user => {
+        if (user.origin in countByOrigin) {
+          countByOrigin[user.origin]++;
+        } else {
+          countByOrigin[user.origin] = 1;
+        }
+      });
+
+      return countByOrigin
+      ;
+    });
+
+    return {
+      userCountByOrigin
+    };
+  }
+});
 </script>
-
-<style>
-
-</style>
