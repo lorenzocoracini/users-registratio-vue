bf738f9ec49cac1964fd529342c81617fc5442f6
lorenzocoracini <lorenzo291909@gmail.com>
2024-04-18 21:33:54 -0300

    Adição de validação para email já existe no cadastro de usuários.

--- src/stores/users.js
+++ src/stores/users.js
@@ -4,10 +4,19 @@ import { defineStore } from "pinia";
 export const useUsersStore = defineStore("users", {
   state: () => ({
     users: [],
+    errorMessage: '', 
   }),
   actions: {
     addUser(user) {
+      const existingUser = this.users.find(u => u.email === user.email);
+      if (existingUser) {
+        this.errorMessage = 'E-mail já cadastrado.';
+        return
+      }
+
       this.users.push(user);
+      this.errorMessage = '';
+      localStorage.setItem("users", JSON.stringify(this.users));
     },
     getUsers() {
       let items = localStorage.getItem("users");
