e57058947667904b851619966133a50587892216
lorenzocoracini <lorenzo291909@gmail.com>
2024-04-19 14:54:36 -0300

    Adição de grafico de barra no componente de usuários por estado

--- src/components/UserByState.vue
+++ src/components/UserByState.vue
@@ -8,35 +8,55 @@
           <v-col cols="6">{{ count }}</v-col>
         </v-row>
       </v-container>
+      <apexchart type="bar" :options="chartOptions" :series="chartSeries" height="350" />
     </v-card-text>
   </v-card>
 </template>
 
 <script>
+import VueApexCharts from 'vue3-apexcharts';
 import { useUsersStore } from '@/stores/users';
 import { defineComponent, computed } from 'vue';
 
 export default defineComponent({
+  components: {
+    apexchart: VueApexCharts
+  },
   setup() {
     const usersStore = useUsersStore();
-
-    const userCountByState = computed(() => {
+    const userDataByState = computed(() => {
       const users = usersStore.users;
-      const countByState = {};
+      const dataByState = {};
 
       users.forEach(user => {
-        if (user.state in countByState) {
-          countByState[user.state]++;
+        if (user.state in dataByState) {
+          dataByState[user.state]++;
         } else {
-          countByState[user.state] = 1;
+          dataByState[user.state] = 1;
         }
       });
 
-      return countByState;
+      return dataByState;
+    });
+
+    const chartOptions = computed(() => {
+      return {
+        xaxis: {
+          categories: Object.keys(userDataByState.value)
+        }
+      };
+    });
+
+    const chartSeries = computed(() => {
+      return [{
+        name: 'Usuários por Estado',
+        data: Object.values(userDataByState.value)
+      }];
     });
 
     return {
-      userCountByState
+      chartOptions,
+      chartSeries
     };
   }
 });
